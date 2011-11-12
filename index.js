a24288c374a5d215d258638fd42c8dabb5008116
Alejandro Morales <vamg008@gmail.com>
2011-11-12 00:40:09 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,1 @@
+var ObjectId = require('./lib/ObjectID').ObjectId;
---
+++ test/test.js
@@ -0,0 +1,20 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    ObjectId = require('../lib/ObjectID').ObjectId,
+    test;
+
+console.log(new ObjectId().toString());
+for (var i=0;i<1000;i++){
+  test = new ObjectId().toString();
+  vows.describe('Generate an ObjectId like BSON spec ' + i).addBatch({
+      'An Object': {
+          topic: test,
+          'is string': function (test) {
+              assert.isString(test);
+          },
+          'and 24 chars length': function (test) {
+             assert.equal(test.length,24) 
+          }
+      }
+  }).export(module)  
+}
